04fdd548811be67dab186490c093e64c4482f357
Leonardo Rebouças Batista <leo292629@gmail.com>
2020-08-03 14:10:42 -0300

    full app working

---
+++ src/config/dev.ts
@@ -0,0 +1,20 @@
+export const mongoURI =
+  'mongodb+srv://myuser:VHq4y3G7zjUYJQzx@cluster0.qmx9x.mongodb.net/emaily?retryWrites=true&w=majority';
+// 'mongodb+srv://myuser:k41mrIRzZiSEGsEc@cluster0.8rshs.mongodb.net/emailly?retryWrites=true&w=majority';
+
+export const googleClientID =
+  '462839699228-b1iu20irc5g81v64plhr6lsin3m7toh2.apps.googleusercontent.com';
+
+export const googleClientSecret = 'fTEJ_rk95RGaesbMjbto1yDB';
+
+export const cookieKey = 'adsedas';
+
+export const googleProxy = 'http://localhost:5000';
+
+export const clientURI = 'http://localhost:3000';
+
+export const stripePublishableKey = 'pk_test_Gi5ry9vOhLbCtkBmkchRqfuX';
+export const stripeSecretKey = 'sk_test_dyt5OwJumEgt3Fu7Ea0XAqGH';
+export const sendGridKey =
+  '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';
+export const redirectDomain = 'http://localhost:3000';
