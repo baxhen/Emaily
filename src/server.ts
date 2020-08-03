418eb6233e93ff7b24f0f0720023db66d0e40356
Leonardo Rebouças Batista <leo292629@gmail.com>
2020-08-03 13:54:03 -0300

    Users can see surveys results

--- src/server.ts
+++ src/server.ts
@@ -52,10 +52,10 @@ app.use(billingRoutes);
 app.use(surveyRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
